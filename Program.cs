69c77769065db4ab8f68ee4ccbf69d09833c864a
Alan <alansantosptrainer@hotmail.com>
2026-04-02 11:50:12 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using JogodeRPG.src.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Knight hero = new Knight ("Arus", 23, "Knight");
+        Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+       
+       
+         
+         Console.WriteLine(wizard.Attack(1));
+         Console.WriteLine(wizard.Attack(7));
+    }
+}
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogodeRPG.src.Entities
+{
+    public class Wizard : Hero
+    {
+
+        public Wizard(string Name, int Level, string HeroType) 
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        
+      public override string Attack()
+        {
+            return this.Name + " Atacou com sua magia";
+        }
+
+        public string Attack(int Bonus)
+
+        {
+            if (Bonus > 6)
+            {
+                return this.Name + "Lançou magia com bônus de ataque de " + Bonus;
+            }
+            else
+            {
+                return this.Name + "Lançou magia com força fraca com bônus de ataque de " + Bonus;
+            }
+        }
+
+
+    }
+}
